24b00e7e58745ac795c6fe19295ef309efa8fb31
Vil <vilghost@gmail.com>
2016-11-27 16:42:16 +0100

    DefaultSolve finished

--- src/main/java/DefaultSolve.java
+++ src/main/java/DefaultSolve.java
@@ -1,44 +1,38 @@
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.variables.BoolVar;
 import org.chocosolver.solver.variables.IntVar;
-
-import java.util.ArrayList;
 
 public class DefaultSolve implements ISolve {
 
     private int n = 5;
     private Model model = new Model("Backpack problem with "+n+" objects.");
 
-    private IntVar[] isPickedArray = new IntVar[n];
-    private int[] weight = new int[n];
+
+    IntVar[] occurences = new IntVar[n];
+    IntVar weightSum = null, energySum = null;
+    int[] weight = {2, 5, 1, 10, 5}, energy = {6, 4, 2, 8, 5};
+    private int capacity = 30;
 
     public void defineModel() {
 
-        ArrayList<Item> items = new ArrayList<Item>();
 
-        weight[0] = 1;
-        weight[1] = 10;
-        weight[2] = 4;
-        weight[3] = 3;
-        weight[4] = 2;
 
-        //Initialisation des objets dans le problème
         for(int i = 0; i < n; i++){
-            isPickedArray[i] = model.intVar("P"+i, 0, 1);
+            occurences[i] = model.intVar("O"+i, 0, 1);
         }
+        weightSum = model.intVar("ws", 0, n*100);
+        energySum = model.intVar("es", 0, n*100);
 
-        //Initialisation de la capacité du sac à dos
-        int capacity = 2;
+        model.scalar(occurences, weight, "=", weightSum).post();
+        model.scalar(occurences, energy, "=", energySum).post();
 
-        model.scalar(isPickedArray, weight, "<=", capacity).post();
-
+        model.arithm(weightSum, "<=", capacity).post();
 
     }
 
     public void solve() {
 
-        Solution solution = model.getSolver().findSolution();//.findOptimalSolution(utility, true);
+        Solution solution = model.getSolver().findOptimalSolution(energySum, true);
         if(solution != null){
             System.out.println(solution.toString());
         }
